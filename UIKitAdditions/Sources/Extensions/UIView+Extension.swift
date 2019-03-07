6d0007c626c8b931b8df7c9133ae1b6bd806c8b1
Prince Ugwuh <prince.ugwuh@gmail.com>
2019-03-07 13:40:59 -0600

    made public rounded corners function

--- UIKitAdditions/Sources/Extensions/UIView+Extension.swift
+++ UIKitAdditions/Sources/Extensions/UIView+Extension.swift
@@ -38,7 +38,7 @@ extension UIView {
 }
 
 extension UIView {
-    func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
+    public func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
         let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
         let mask = CAShapeLayer()
         mask.path = path.cgPath
