cfa7f24aadf8bbdd55ed2c2510503c6d13dd17d8
Prince Ugwuh <prince.ugwuh@gmail.com>
2019-03-19 21:00:50 -0500

    missed public params for RequestConvertible

--- UIKitAdditions/Sources/Utils/RequestConvertible.swift
+++ UIKitAdditions/Sources/Utils/RequestConvertible.swift
@@ -38,7 +38,7 @@ extension RequestConvertible where Params: Encodable {
 }
 
 extension RequestConvertible where Self: Encodable {
-    var params: Self {
+    public var params: Self {
         return self
     }
 }
