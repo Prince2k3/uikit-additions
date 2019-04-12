39ea193a37c49d384870a463dafd7bde310b12ba
Prince Ugwuh <prince.ugwuh@gmail.com>
2019-04-11 23:13:23 -0500

    Updates to GradientView

--- UIKitAdditions/Sources/UI/Views/GradientView.swift
+++ UIKitAdditions/Sources/UI/Views/GradientView.swift
@@ -1,27 +1,25 @@
 import UIKit
 
 public class GradientView: UIView {
-    @IBInspectable
-    public var startColor: UIColor = .white {
+    @IBInspectable public var startColor: UIColor = .white {
         didSet {
             updateView()
         }
     }
 
-    @IBInspectable
-    public var endColor: UIColor = .black {
+    @IBInspectable public var endColor: UIColor = .black {
         didSet {
             updateView()
         }
     }
 
-    public var startPoint: CGPoint = .zero {
+    public var startPoint: Point = .topLeft {
         didSet {
             updateView()
         }
     }
 
-    public var endPoint: CGPoint = CGPoint(x: 0.0, y: 1.0) {
+    public var endPoint: Point = .bottomLeft {
         didSet {
             updateView()
         }
@@ -31,23 +29,63 @@ public class GradientView: UIView {
         return CAGradientLayer.self
     }
 
-    public convenience init(startColor: UIColor, endColor: UIColor) {
+    public convenience init(startColor: UIColor, startPoint: Point = .topLeft, endColor: UIColor, endPoint: Point = .bottomLeft) {
         self.init()
         self.startColor = startColor
+        self.startPoint = startPoint
         self.endColor = endColor
+        self.endPoint = endPoint
+        
         updateView()
     }
 
     private var gradientLayer: CAGradientLayer {
-        guard let layer = self.layer as? CAGradientLayer else {
-            fatalError("layer is not of type CAGradientLayer")
-        }
+        guard
+            let layer = self.layer as? CAGradientLayer
+            else { fatalError("layer is not of type CAGradientLayer") }
         return layer
     }
 
     private func updateView() {
-        self.gradientLayer.startPoint = self.startPoint
-        self.gradientLayer.endPoint = self.endPoint
+        self.gradientLayer.startPoint = self.startPoint.point
+        self.gradientLayer.endPoint = self.endPoint.point
         self.gradientLayer.colors = [self.startColor.cgColor, self.endColor.cgColor]
+    }
+}
+
+extension GradientView {
+    public enum Point {
+        case topLeft
+        case centerLeft
+        case bottomLeft
+        case topCenter
+        case center
+        case bottomCenter
+        case topRight
+        case centerRight
+        case bottomRight
+        
+        var point: CGPoint {
+            switch self {
+            case .topLeft:
+                return CGPoint(x: 0.0, y: 0)
+            case .centerLeft:
+                return CGPoint(x: 0.0, y: 0.5)
+            case .bottomLeft:
+                return CGPoint(x: 0.0, y: 1.0)
+            case .topCenter:
+                return CGPoint(x: 0.5, y: 0)
+            case .center:
+                return CGPoint(x: 0.5, y: 0.5)
+            case .bottomCenter:
+                return CGPoint(x: 0.5, y: 1.0)
+            case .topRight:
+                return CGPoint(x: 1.0, y: 0.0)
+            case .centerRight:
+                return CGPoint(x: 1.0, y: 0.5)
+            case .bottomRight:
+                return CGPoint(x: 1.0, y: 1.0)
+            }
+        }
     }
 }
