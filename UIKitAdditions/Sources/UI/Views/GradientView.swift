72bd961b615b4a494ab93e588ce0c7b10de42eb4
Prince Ugwuh <prince.ugwuh@gmail.com>
2019-04-17 10:53:49 -0500

    gradient view updates

--- UIKitAdditions/Sources/UI/Views/GradientView.swift
+++ UIKitAdditions/Sources/UI/Views/GradientView.swift
@@ -24,19 +24,34 @@ public class GradientView: UIView {
             updateView()
         }
     }
+    
+    public var colors: [UIColor] = [] {
+        didSet {
+            updateView()
+        }
+    }
+    
+    public var locations: [NSNumber]? {
+        didSet {
+            updateView()
+        }
+    }
 
     public override static var layerClass: AnyClass {
         return CAGradientLayer.self
     }
 
-    public convenience init(startColor: UIColor, startPoint: Point = .topLeft, endColor: UIColor, endPoint: Point = .bottomLeft) {
+    public convenience init(colors: [UIColor], startPoint: Point = .topLeft, endPoint: Point = .bottomLeft) {
         self.init()
-        self.startColor = startColor
+        self.colors = colors
         self.startPoint = startPoint
-        self.endColor = endColor
         self.endPoint = endPoint
         
         updateView()
+    }
+    
+    public convenience init(startColor: UIColor, endColor: UIColor, startPoint: Point = .topLeft, endPoint: Point = .bottomLeft) {
+        self.init(colors: [startColor, endColor], startPoint: startPoint, endPoint: endPoint)
     }
 
     private var gradientLayer: CAGradientLayer {
@@ -47,9 +62,10 @@ public class GradientView: UIView {
     }
 
     private func updateView() {
-        self.gradientLayer.startPoint = self.startPoint.point
-        self.gradientLayer.endPoint = self.endPoint.point
-        self.gradientLayer.colors = [self.startColor.cgColor, self.endColor.cgColor]
+        self.gradientLayer.colors = self.colors
+        self.gradientLayer.locations = self.locations
+        self.gradientLayer.startPoint = self.startPoint.rawValue
+        self.gradientLayer.endPoint = self.endPoint.rawValue
     }
 }
 
@@ -64,28 +80,57 @@ extension GradientView {
         case topRight
         case centerRight
         case bottomRight
-        
-        var point: CGPoint {
-            switch self {
-            case .topLeft:
-                return CGPoint(x: 0.0, y: 0)
-            case .centerLeft:
-                return CGPoint(x: 0.0, y: 0.5)
-            case .bottomLeft:
-                return CGPoint(x: 0.0, y: 1.0)
-            case .topCenter:
-                return CGPoint(x: 0.5, y: 0)
-            case .center:
-                return CGPoint(x: 0.5, y: 0.5)
-            case .bottomCenter:
-                return CGPoint(x: 0.5, y: 1.0)
-            case .topRight:
-                return CGPoint(x: 1.0, y: 0.0)
-            case .centerRight:
-                return CGPoint(x: 1.0, y: 0.5)
-            case .bottomRight:
-                return CGPoint(x: 1.0, y: 1.0)
-            }
+    }
+}
+
+extension GradientView.Point: RawRepresentable {
+    public init?(rawValue: CGPoint) {
+        switch rawValue {
+        case CGPoint(x: 0.0, y: 0):
+            self = .topLeft
+        case CGPoint(x: 0.0, y: 0.5):
+            self = .centerLeft
+        case CGPoint(x: 0.0, y: 1.0):
+            self = .bottomLeft
+        case CGPoint(x: 0.5, y: 0):
+            self = .topCenter
+        case CGPoint(x: 0.5, y: 0.5):
+            self = .center
+        case CGPoint(x: 0.5, y: 1.0):
+            self = .bottomCenter
+        case CGPoint(x: 1.0, y: 0.0):
+            self = .topRight
+        case CGPoint(x: 1.0, y: 0.5):
+            self = .centerRight
+        case CGPoint(x: 1.0, y: 1.0):
+            self = .bottomRight
+        default:
+            return nil
         }
     }
+    
+    public var rawValue: CGPoint {
+        switch self {
+        case .topLeft:
+            return CGPoint(x: 0.0, y: 0)
+        case .centerLeft:
+            return CGPoint(x: 0.0, y: 0.5)
+        case .bottomLeft:
+            return CGPoint(x: 0.0, y: 1.0)
+        case .topCenter:
+            return CGPoint(x: 0.5, y: 0)
+        case .center:
+            return CGPoint(x: 0.5, y: 0.5)
+        case .bottomCenter:
+            return CGPoint(x: 0.5, y: 1.0)
+        case .topRight:
+            return CGPoint(x: 1.0, y: 0.0)
+        case .centerRight:
+            return CGPoint(x: 1.0, y: 0.5)
+        case .bottomRight:
+            return CGPoint(x: 1.0, y: 1.0)
+        }
+    }
+    
+    public typealias RawValue = CGPoint
 }
