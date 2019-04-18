5e7e5593ab08ddae5094786a69cd9fa443bf8fb9
Prince Ugwuh <prince.ugwuh@gmail.com>
2019-04-18 10:26:28 -0500

    revert didnt work

--- UIKitAdditions/Sources/Extensions/UINavigationController+Extension.swift
+++ UIKitAdditions/Sources/Extensions/UINavigationController+Extension.swift
@@ -10,63 +10,63 @@ extension UINavigationController {
     }
 }
 
-//extension UINavigationController {
-//    open override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
-//        return self.topViewController?.preferredInterfaceOrientationForPresentation ?? super.preferredInterfaceOrientationForPresentation
-//    }
-//
-////    open override var transitioningDelegate: UIViewControllerTransitioningDelegate? {
-////        get {
-////            return self.topViewController?.transitioningDelegate ?? super.transitioningDelegate
-////        }
-////
-////        set {
-////
-////            if let topViewController = self.topViewController {
-////                topViewController.transitioningDelegate = newValue
-////            } else {
-////               super.transitioningDelegate = newValue
-////            }
-////        }
-////    }
-//
-//    open override var preferredStatusBarStyle: UIStatusBarStyle {
-//        return self.topViewController?.preferredStatusBarStyle ?? super.preferredStatusBarStyle
-//    }
-//
-//    open override var prefersStatusBarHidden: Bool {
-//        return self.topViewController?.prefersStatusBarHidden ?? super.prefersStatusBarHidden
-//    }
-//
-////    open override var modalPresentationStyle: UIModalPresentationStyle {
-////        get {
-////            return self.topViewController?.modalPresentationStyle ?? super.modalPresentationStyle
-////        }
-////
-////        set {
-////            if let topViewController = self.topViewController {
-////                topViewController.modalPresentationStyle = newValue
-////            } else {
-////               super.modalPresentationStyle = newValue
-////            }
-////        }
-////    }
-////
-////    open override var modalTransitionStyle: UIModalTransitionStyle {
-////        get {
-////            return self.topViewController?.modalTransitionStyle ?? super.modalTransitionStyle
-////        }
-////
-////        set {
-////            if let topViewController = self.topViewController {
-////                topViewController.modalTransitionStyle = newValue
-////            } else {
-////                super.modalTransitionStyle = newValue
-////            }
-////        }
-////    }
-//
-//    open override var shouldAutorotate: Bool {
-//        return self.topViewController?.shouldAutorotate ?? super.shouldAutorotate
-//    }
-//}
+extension UINavigationController {
+    open override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+        return self.topViewController?.preferredInterfaceOrientationForPresentation ?? super.preferredInterfaceOrientationForPresentation
+    }
+
+    open override var transitioningDelegate: UIViewControllerTransitioningDelegate? {
+        get {
+            return self.topViewController?.transitioningDelegate ?? super.transitioningDelegate
+        }
+
+        set {
+
+            if let topViewController = self.topViewController {
+                topViewController.transitioningDelegate = newValue
+            } else {
+               super.transitioningDelegate = newValue
+            }
+        }
+    }
+
+    open override var preferredStatusBarStyle: UIStatusBarStyle {
+        return self.topViewController?.preferredStatusBarStyle ?? super.preferredStatusBarStyle
+    }
+
+    open override var prefersStatusBarHidden: Bool {
+        return self.topViewController?.prefersStatusBarHidden ?? super.prefersStatusBarHidden
+    }
+
+    open override var modalPresentationStyle: UIModalPresentationStyle {
+        get {
+            return self.topViewController?.modalPresentationStyle ?? super.modalPresentationStyle
+        }
+
+        set {
+            if let topViewController = self.topViewController {
+                topViewController.modalPresentationStyle = newValue
+            } else {
+               super.modalPresentationStyle = newValue
+            }
+        }
+    }
+
+    open override var modalTransitionStyle: UIModalTransitionStyle {
+        get {
+            return self.topViewController?.modalTransitionStyle ?? super.modalTransitionStyle
+        }
+
+        set {
+            if let topViewController = self.topViewController {
+                topViewController.modalTransitionStyle = newValue
+            } else {
+                super.modalTransitionStyle = newValue
+            }
+        }
+    }
+
+    open override var shouldAutorotate: Bool {
+        return self.topViewController?.shouldAutorotate ?? super.shouldAutorotate
+    }
+}
