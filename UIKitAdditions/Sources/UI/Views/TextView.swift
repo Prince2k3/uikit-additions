95fd1179334cbf3f62e8189009b2f738fca65036
Prince Ugwuh <prince.ugwuh@gmail.com>
2019-04-27 12:05:06 -0500

    update Alamofire dep. Added TextView

---
+++ UIKitAdditions/Sources/UI/Views/TextView.swift
@@ -0,0 +1,102 @@
+//
+//  TextView.swift
+//  UIKitAdditions
+//
+//  Created by Prince Ugwuh on 4/27/19.
+//  Copyright © 2019 Prince Ugwuh. All rights reserved.
+//
+
+import Foundation
+
+class TextView: UITextView {
+    private let placeholderTextView: UITextView = {
+        let view = UITextView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
+        view.isScrollEnabled = false
+        view.isUserInteractionEnabled = false
+        view.textColor = .gray
+        return view
+    }()
+    
+    var maxHeight: CGFloat = 0.0
+    var placeholder: String? {
+        get { return self.placeholderTextView.text }
+        set { self.placeholderTextView.text = newValue }
+    }
+    
+    var placeholderTextColor: UIColor = .gray {
+        didSet {
+            self.placeholderTextView.textColor = self.placeholderTextColor
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        NotificationCenter.default.addObserver(self, selector: #selector(textDidChange), name: UITextView.textDidChangeNotification, object: self)
+        
+        self.isScrollEnabled = false
+        self.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        self.placeholderTextView.font = self.font
+        addSubview(self.placeholderTextView)
+        
+        NSLayoutConstraint.activate([
+            self.placeholderTextView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.placeholderTextView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.placeholderTextView.topAnchor.constraint(equalTo: self.topAnchor),
+            self.placeholderTextView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+        ])
+    }
+    
+    override var text: String! {
+        didSet {
+            invalidateIntrinsicContentSize()
+            self.placeholderTextView.isHidden = !self.text.isEmpty
+        }
+    }
+    
+    override var font: UIFont? {
+        didSet {
+            self.placeholderTextView.font = self.font
+            invalidateIntrinsicContentSize()
+        }
+    }
+    
+    override var contentInset: UIEdgeInsets {
+        didSet {
+            self.placeholderTextView.contentInset = self.contentInset
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        var size = super.intrinsicContentSize
+        
+        if size.height == UIView.noIntrinsicMetric {
+            self.layoutManager.glyphRange(for: self.textContainer)
+            size.height = self.layoutManager.usedRect(for: self.textContainer).height + self.textContainerInset.top + self.textContainerInset.bottom
+        }
+        
+        if self.maxHeight > 0.0 && size.height > self.maxHeight {
+            size.height = self.maxHeight
+            
+            if !self.isScrollEnabled {
+                self.isScrollEnabled = true
+            }
+            
+        } else if self.isScrollEnabled {
+            self.isScrollEnabled = false
+        }
+        
+        return size
+    }
+    
+    @objc private func textDidChange(_ notification: Notification) {
+        invalidateIntrinsicContentSize()
+        self.placeholderTextView.isHidden = !self.text.isEmpty
+    }
+}
