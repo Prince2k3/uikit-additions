dfc723e65a8f2528ad666fa8bd33b2fd40d77800
Prince Ugwuh <prince.ugwuh@gmail.com>
2019-05-09 11:48:32 -0500

    updates to textview

--- UIKitAdditions/Sources/UI/Views/TextView.swift
+++ UIKitAdditions/Sources/UI/Views/TextView.swift
@@ -9,94 +9,175 @@
 import Foundation
 
 open class TextView: UITextView {
-    private let placeholderTextView: UITextView = {
-        let view = UITextView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .clear
-        view.isScrollEnabled = false
-        view.isUserInteractionEnabled = false
-        view.textColor = .gray
-        return view
-    }()
-    
-    @IBInspectable public var maxHeight: CGFloat = 0.0
-    @IBInspectable public var placeholder: String? {
-        get { return self.placeholderTextView.text }
-        set { self.placeholderTextView.text = newValue }
+    @IBInspectable var activeTextColor: UIColor = .black
+    @IBInspectable var inactiveTextColor: UIColor = .darkGray
+    @IBInspectable var activeFloatingLabelColor: UIColor = .blue
+    @IBInspectable var inactiveFloatingLabelColor: UIColor = .lightGray
+    @IBInspectable var placeholderTextColor: UIColor = .lightGray {
+        didSet { self.placeholderLabel?.textColor = self.placeholderTextColor }
     }
     
-    @IBInspectable public var placeholderTextColor: UIColor = .gray {
+    @IBInspectable open  var placeholder: String = "" {
         didSet {
-            self.placeholderTextView.textColor = self.placeholderTextColor
+            self.placeholderLabel?.text = self.placeholder
+            self.floatingLabel?.text = self.placeholder
         }
     }
     
-    public required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+    @IBInspectable open  var enableFloatingPlaceholder: Bool = true {
+        didSet {
+            if !self.enableFloatingPlaceholder {
+                hideFloatinglabel(false)
+            }
+        }
     }
     
-    public override init(frame: CGRect, textContainer: NSTextContainer?) {
-        super.init(frame: frame, textContainer: textContainer)
-        NotificationCenter.default.addObserver(self, selector: #selector(textDidChange), name: UITextView.textDidChangeNotification, object: self)
-        
-        self.isScrollEnabled = false
-        self.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        
-        self.placeholderTextView.font = self.font
-        addSubview(self.placeholderTextView)
-        
-        NSLayoutConstraint.activate([
-            self.placeholderTextView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            self.placeholderTextView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            self.placeholderTextView.topAnchor.constraint(equalTo: self.topAnchor),
-            self.placeholderTextView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-        ])
+    open var minimumHeight: CGFloat = 44
+    open var maximumHeight: CGFloat = 9999999
+    open var maxCharacterLength: Int = .max
+    var heightDidUpdate: (() -> Void)?
+    
+    private var heightConstraint: NSLayoutConstraint!
+    private var placeholderLabel: UILabel!
+    private var floatingLabel: UILabel!
+    private var floatingLabelTopConstraint: NSLayoutConstraint!
+    private var floatingLabelVisible: Bool = false
+    private var heightRange: ClosedRange<CGFloat> {
+        return self.minimumHeight...self.maximumHeight
+    }
+    
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+        commonInit()
     }
     
     open override var text: String! {
-        didSet {
-            invalidateIntrinsicContentSize()
-            self.placeholderTextView.isHidden = !self.text.isEmpty
-        }
-    }
-    
-    open override var font: UIFont? {
-        didSet {
-            self.placeholderTextView.font = self.font
-            invalidateIntrinsicContentSize()
-        }
-    }
-    
-    open override var contentInset: UIEdgeInsets {
-        didSet {
-            self.placeholderTextView.contentInset = self.contentInset
-        }
+        didSet { update() }
     }
     
     open override var intrinsicContentSize: CGSize {
-        var size = super.intrinsicContentSize
-        
-        if size.height == UIView.noIntrinsicMetric {
-            self.layoutManager.glyphRange(for: self.textContainer)
-            size.height = self.layoutManager.usedRect(for: self.textContainer).height + self.textContainerInset.top + self.textContainerInset.bottom
-        }
-        
-        if self.maxHeight > 0.0 && size.height > self.maxHeight {
-            size.height = self.maxHeight
-            
-            if !self.isScrollEnabled {
-                self.isScrollEnabled = true
-            }
-            
-        } else if self.isScrollEnabled {
-            self.isScrollEnabled = false
-        }
-        
-        return size
+        var contentSize = self.contentSize
+        contentSize.height += (self.textContainerInset.bottom + self.textContainerInset.top) / 2
+        return contentSize
     }
     
-    @objc private func textDidChange(_ notification: Notification) {
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if self.heightConstraint != nil {
+            let height = self.heightRange.clamp(self.intrinsicContentSize.height)
+            
+            if self.maximumHeight != height {
+                self.contentOffset = .zero
+            }
+            
+            self.heightConstraint.constant = height
+            self.heightDidUpdate?()
+            
+        } else {
+            self.contentOffset = CGPoint(x: 0, y: -8) // HACK ALERT: weird workaroud for text not starting at the top of the view.
+        }
+    }
+    
+    open func commonInit() {
+        NotificationCenter.default.addObserver(self, selector: #selector(textDidChange), name: UITextView.textDidChangeNotification, object: nil)
+        
+        self.placeholderLabel = UILabel()
+        addSubview(self.placeholderLabel)
+        self.placeholderLabel.numberOfLines = 0
+        self.placeholderLabel.font = self.font
+        self.placeholderLabel.textColor = self.placeholderTextColor
+        self.placeholderLabel.backgroundColor = .clear
+        self.placeholderLabel.text = self.placeholder
+        self.placeholderLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.placeholderLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 5).isActive = true
+        self.placeholderLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 8).isActive = true
+        self.placeholderLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -5).isActive = true
+        
+        self.floatingLabel = UILabel()
+        addSubview(self.floatingLabel)
+        
+        self.floatingLabel.textAlignment = self.textAlignment
+        self.floatingLabel.textColor = self.activeFloatingLabelColor
+        
+        if let font = self.font {
+            self.floatingLabel.font = UIFont(name: font.fontName, size: (max(12, font.pointSize * 0.65)))
+        }
+        
+        self.floatingLabel.text = self.placeholder
+        self.floatingLabel.alpha = 0
+        self.floatingLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.floatingLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 4).isActive = true
+        self.floatingLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -4).isActive = true
+        self.floatingLabelTopConstraint = self.floatingLabel.topAnchor.constraint(equalTo: self.topAnchor)
+        self.floatingLabelTopConstraint?.isActive = true
+        
+        self.clipsToBounds = false
+        
+        for constraint in self.constraints where constraint.firstAttribute == .height {
+            self.heightConstraint = constraint
+            self.minimumHeight = constraint.constant
+        }
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UITextView.textDidChangeNotification, object: nil)
+    }
+    
+    func update() {
+        if let text = self.text, !text.isEmpty {
+            self.placeholderLabel.isHidden = true
+            showFloatingLabel()
+            
+            if text.count > self.maxCharacterLength {
+                self.text = text.substring(0, length: self.maxCharacterLength)
+            }
+        } else {
+            self.placeholderLabel.isHidden = false
+            hideFloatinglabel()
+        }
+    }
+    
+    @objc func textDidChange(_ notification: Notification) {
         invalidateIntrinsicContentSize()
-        self.placeholderTextView.isHidden = !self.text.isEmpty
+        update()
+        layoutIfNeeded()
+    }
+    
+    open func showFloatingLabel(_ animated: Bool = true) {
+        if self.floatingLabelVisible || !self.enableFloatingPlaceholder {
+            return
+        }
+        
+        self.floatingLabelVisible = true
+        self.floatingLabel?.textColor = self.isFirstResponder ? self.activeFloatingLabelColor : self.inactiveFloatingLabelColor
+        self.floatingLabelTopConstraint?.constant = -8
+        
+        let animator = UIViewPropertyAnimator(duration: animated ? 0.3 : 0, curve: .easeInOut) {
+            self.floatingLabel?.alpha = 1
+            self.layoutIfNeeded()
+        }
+        animator.startAnimation()
+    }
+    
+    open func hideFloatinglabel(_ animated: Bool = true) {
+        if !self.floatingLabelVisible || !self.enableFloatingPlaceholder {
+            return
+        }
+        
+        self.floatingLabelVisible = false
+        self.floatingLabelTopConstraint?.constant = 0
+        
+        let animator = UIViewPropertyAnimator(duration: animated ? 0.3 : 0, curve: .easeInOut) {
+            self.floatingLabel?.alpha = 0
+            self.layoutIfNeeded()
+        }
+        animator.startAnimation()
+    }
+}
+
+fileprivate extension ClosedRange {
+    func clamp(_ element: Bound) -> Bound {
+        return Swift.max(lowerBound, Swift.min(upperBound, element))
     }
 }
