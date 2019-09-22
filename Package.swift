246ce1d7f42a8569896c5f1ff20838266c5d98d0
Prince Ugwuh <prince.ugwuh@gmail.com>
2019-09-21 23:06:25 -0500

    removed code in package.swift

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "UIKitAdditions",
-    platforms: [
-        .iOS(.v11.4)
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
