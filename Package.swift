84585b085637d9daaff608e88a91fa7a07f398b4
Prince Ugwuh <prince.ugwuh@gmail.com>
2019-09-22 11:18:34 -0500

    package.swift updates

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "UIKitAdditions",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -25,7 +28,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "UIKitAdditions",
-            dependencies: []),
+            dependencies: ["PromiseKit", "DataSource", "SwiftAdditions", "PhoneNumberKit", "SwiftDate", "Alamofire"]),
         .testTarget(
             name: "UIKitAdditionsTests",
             dependencies: ["UIKitAdditions"]),
