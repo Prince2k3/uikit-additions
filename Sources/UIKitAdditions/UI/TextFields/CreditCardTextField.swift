40075c1f67ccd1ae3336013dad8f631ee0b71edf
Prince Ugwuh <prince.ugwuh@gmail.com>
2019-10-01 12:52:56 -0500

    fixed build error

--- Sources/UIKitAdditions/UI/TextFields/CreditCardTextField.swift
+++ Sources/UIKitAdditions/UI/TextFields/CreditCardTextField.swift
@@ -23,9 +23,8 @@ class CreditCardTextField: TextField {
     }
 
     private func formatCreditCard(_ value: inout String) {
-        let matchedCCs = CreditCardFormatter.all.compactMap { $0.hasPrefix(value) }.sorted { $0.1 < $1.1 }.map { $0.0 }.first
-        let cc = matchedCCs.first
-        iconImage = cc?.image ?? CreditCardFormatter.unknown.image
-        value = cc?.formatString(value) ?? value
+        let matched = CreditCardFormatter.all.compactMap { $0.hasPrefix(value) }.sorted { $0.1 < $1.1 }.map { $0.0 }.first
+        iconImage = matched?.image ?? CreditCardFormatter.unknown.image
+        value = matched?.formatString(value) ?? value
     }
 }
