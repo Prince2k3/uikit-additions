631931af4b684d1fac8562a2982f7ac8cf5660b6
Prince Ugwuh <prince.ugwuh@gmail.com>
2019-10-12 10:30:51 -0500

    BusyButton updates

--- Sources/UIKitAdditions/UI/Buttons/BusyButton.swift
+++ Sources/UIKitAdditions/UI/Buttons/BusyButton.swift
@@ -2,54 +2,79 @@ import UIKit
 
 public class BusyButton: Button {
     private lazy var activityIndicatorView: UIActivityIndicatorView = {
-        let view = UIActivityIndicatorView(style: self.activityIndicatorStyle)
+        let view = UIActivityIndicatorView(style:  activityIndicatorStyle)
         view.translatesAutoresizingMaskIntoConstraints = false
         view.startAnimating()
         return view
     }()
 
+    public var isBusy: Bool = false {
+        didSet {
+            if isBusy {
+                showLoader()
+            } else {
+                hideLoader()
+            }
+        }
+    }
+    
     public var activityIndicatorStyle: UIActivityIndicatorView.Style = .gray {
         didSet {
-            self.activityIndicatorView.style = self.activityIndicatorStyle
+            activityIndicatorView.style = activityIndicatorStyle
         }
     }
 
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-        commonInit()
+    private func showLoader() {
+        guard
+            !subviews.contains(activityIndicatorView)
+            else { return }
+        
+        activityIndicatorView.radius = min(0.7 * frame.height / 2, activityIndicatorView.radius)
+        self.isUserInteractionEnabled = false
+        UIView.transition(with: self, duration: 0.2, options: .curveEaseOut, animations: {
+            self.titleLabel?.alpha = 0.0
+            self.imageView?.alpha = 0.0
+        }) { [weak self] _ in
+            guard let self = self else { return }
+            self.addSubview(self.activityIndicatorView)
+            if self.isBusy {
+                self.activityIndicatorView.startAnimating()
+            } else {
+                self.hideLoader()
+            }
+        }
     }
-
-    public required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+    
+    private func hideLoader() {
+        guard
+            subviews.contains(activityIndicatorView)
+            else { return }
+        isUserInteractionEnabled = true
+        activityIndicatorView.stopAnimating()
+        activityIndicatorView.removeFromSuperview()
+        UIView.transition(with: self, duration: 0.2, options: .curveEaseIn, animations: {
+            self.titleLabel?.alpha = 1.0
+            self.imageView?.alpha = 1.0
+        }) { _ in }
     }
-
-    public override func awakeFromNib() {
-        super.awakeFromNib()
-        commonInit()
-    }
-
-    func commonInit() {
-        setTitleColor(.lightGray, for: .disabled)
-        setBackgroundColor(.lightGray, for: .disabled)
-
-        addSubview(self.activityIndicatorView)
-
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        
         if let titleLabel = self.titleLabel {
             self.activityIndicatorView.leftAnchor.constraint(equalTo: titleLabel.rightAnchor, constant: 8).isActive = true
         } else {
             self.activityIndicatorView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         }
-
-        self.activityIndicatorView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        self.activityIndicatorView.stopAnimating()
     }
+}
 
-    public var isBusy: Bool = false {
-        didSet {
-            self.isEnabled = !self.isBusy
-            self.isBusy ?
-            self.activityIndicatorView.startAnimating() :
-            self.activityIndicatorView.stopAnimating()
+extension UIActivityIndicatorView {
+    public var radius: CGFloat {
+        get { return self.frame.width / 2 }
+        set {
+            frame.size = CGSize(width: 2 * newValue, height: 2 * newValue)
+            setNeedsDisplay()
         }
     }
 }
